bc234a55ef1ea26a91435856bd1f0d64d889a6ea
Guto Maia <guto@guto.net>
2024-03-27 15:34:48 -0300

    tests: deprecation: s/assertEquals/assertEqual/g

--- lexical/tests/sql_test.py
+++ lexical/tests/sql_test.py
@@ -19,25 +19,25 @@ class SQLTest(unittest.TestCase):
         query = 'SELECT * FROM table'
         analysis = lexical.analyse(query, sql_tokens)
         tokens = list(analysis)
-        self.assertEquals(4, len(tokens))
-        self.assertEquals('T_SELECT', tokens[0]['type'])
-        self.assertEquals('T_WILDCARD', tokens[1]['type'])
-        self.assertEquals('T_FROM', tokens[2]['type'])
-        self.assertEquals('T_LABEL', tokens[3]['type'])
+        self.assertEqual(4, len(tokens))
+        self.assertEqual('T_SELECT', tokens[0]['type'])
+        self.assertEqual('T_WILDCARD', tokens[1]['type'])
+        self.assertEqual('T_FROM', tokens[2]['type'])
+        self.assertEqual('T_LABEL', tokens[3]['type'])
 
     def test_select_all_fields_with_lower_case(self):
         query = 'select * from table'
         analysis = lexical.analyse(query, sql_tokens)
         tokens = list(analysis)
-        self.assertEquals(4, len(tokens))
-        self.assertEquals('T_SELECT', tokens[0]['type'])
-        self.assertEquals('T_WILDCARD', tokens[1]['type'])
-        self.assertEquals('T_FROM', tokens[2]['type'])
-        self.assertEquals('T_LABEL', tokens[3]['type'])
+        self.assertEqual(4, len(tokens))
+        self.assertEqual('T_SELECT', tokens[0]['type'])
+        self.assertEqual('T_WILDCARD', tokens[1]['type'])
+        self.assertEqual('T_FROM', tokens[2]['type'])
+        self.assertEqual('T_LABEL', tokens[3]['type'])
 
     def test_select_all_fields_with_lower_case(self):
         query = 'selectfrom'
         analysis = lexical.analyse(query, sql_tokens)
         tokens = list(analysis)
-        # self.assertEquals(1, len(tokens))
-        self.assertEquals('T_LABEL', tokens[0]['type'])
+        # self.assertEqual(1, len(tokens))
+        self.assertEqual('T_LABEL', tokens[0]['type'])
