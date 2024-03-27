ed7c39fc1a36ccd6e853583de0f4dde6fce5ce54
Guto Maia <guto@guto.net>
2024-03-27 15:41:29 -0300

    tests: removed unused test

--- lexical/tests/sql_test.py
+++ lexical/tests/sql_test.py
@@ -32,10 +32,3 @@ class SQLTest(unittest.TestCase):
         self.assertEqual('T_WILDCARD', tokens[1]['type'])
         self.assertEqual('T_FROM', tokens[2]['type'])
         self.assertEqual('T_LABEL', tokens[3]['type'])
-
-    def test_select_all_fields_with_lower_case(self):
-        query = 'selectfrom'
-        analysis = lexical.analyse(query, sql_tokens)
-        tokens = list(analysis)
-        # self.assertEqual(1, len(tokens))
-        self.assertEqual('T_LABEL', tokens[0]['type'])
